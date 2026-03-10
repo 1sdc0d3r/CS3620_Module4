033afdff3f14f6f5b9c6e9956c4e33e60995cfa0
Braden <fish0859@gmail.com>
2026-03-09 19:34:46 -0600

    categories and tags setup, stuck on admin interfaces due to instruction

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import Product
-# Register your models here.
+from .models import Product, Category, Tag
 
-admin.site.register(Product)
+class inlineTagModel(admin.StackedInline): # or TabularInline
+    model = Tag
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("categoryy",)
+    # inlines = [inlineTagModel]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("category",)
+
+class TagAdmin(admin.ModelAdmin):
+    list_filter = ("tags",)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Tag, TagAdmin)
+admin.site.register(Tag)
--- store/models.py
+++ store/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 
-# Create your models here.
+#One-to-Many
+class Category(models.Model):
+    category = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.category
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+# Many-to-many
+class Tag(models.Model):
+    tag = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.tag
+
+
 class Product(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    tagss = models.ManyToManyField(Tag)
+    categoryy = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, related_name='products')
 
     def __str__(self):
         return self.name
